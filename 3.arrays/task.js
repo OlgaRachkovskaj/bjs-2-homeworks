046b117869a14d9e2c6a59863424235008ed3482
Olga Rachkovskaj <volchek_o.k@mail.ru>
2023-02-10 07:27:50 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    let result;
+ 
+    return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
+    
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(users, gender){
+
+if (users.length === 0){
+    return 0;
+} else if (gender === "мужской"){
+	const man = users.filter(obj => obj.gender === "мужской")
+	let resultM = man.reduce((r,man) => r +  man.age, 0) / man.length;
+	return resultM;
+} else if(gender === "женский"){
+	const women = users.filter(obj => obj.gender === "женский")
+	let resultW = women.reduce((r,women) => r +  women.age, 0) / women.length;
+	return resultW;
+} else if(gender === "инопланетянин"){
+	return 0;
+}
 }

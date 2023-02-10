b2997ce617b26aa2ec76ed4d2a33a131017afebb
Olga Rachkovskaj <volchek_o.k@mail.ru>
2023-02-10 13:45:44 +0300

    Третье задние выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    let result;
+ 
+    return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
+    
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (users.length === 0) return 0;
+
+let result = users.filter(users => users.gender === gender).reduce((res, item, index, array) => res + item.age / array.length, 0); 
+return result;
 }

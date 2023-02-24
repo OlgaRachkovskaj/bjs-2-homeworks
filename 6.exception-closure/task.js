dbd55b28d90935681b8e181eb000625eb53f5ba0
Olga Rachkovskaj <volchek_o.k@mail.ru>
2023-02-24 16:43:06 +0300

    Шестое задание выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,66 @@
-﻿
+﻿/*Задача1*/
+function parseCount (n)
+        {
+            let parseResult = Number.parseFloat(n);
+            if (Number.isNaN(parseResult)) throw new Error("Невалидное значение");
+            return parseResult;
+        }
+
+ 
+    function validateCount(n) {
+        try {
+            return parseCount(n);
+        } catch (e)  {
+            return e;
+        }
+    }
+
+ 
+ /*Задача2*/
+
+   class Triangle
+        {
+            constructor(a, b, c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+                if (a > b + c || b > a + c || c > a + b)
+                {
+                    throw new Error("Треугольник с такими сторонами не существует")
+                }
+                
+            }
+
+                get perimeter ()
+            {
+                return (this.a + this.b + this.c);
+            }
+ 
+                get area ()
+            {
+       
+                const p = this.perimeter / 2;
+                return Number(((p * (p - this.a) * (p - this.b) * (p - this.c)) ** 0.5).toFixed(3));
+            }
+        }
+
+
+      function getTriangle (a, b, c)
+        {
+            try
+            {
+                return new Triangle(a, b, c);
+            } catch (e){
+            	let errMsg = "Ошибка! Треугольник не существует";
+            	return {
+            		get perimeter(){
+            			return errMsg;
+            		},
+            		get area(){
+            			return errMsg;
+            		}
+            }
+                      
+          }
+        } 

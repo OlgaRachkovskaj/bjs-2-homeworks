ba66cae3d7b0c16a0cc7f6a85e1420368f86d42c
Olga Rachkovskaj <volchek_o.k@mail.ru>
2023-02-26 14:33:56 +0300

    восьмое задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,54 @@
 //Задача № 1
+
+
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+    let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1 ) { // если элемент не найден
+        console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+        return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+    if (cache.length > 5) { 
+      cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+
+function debounceDecoratorNew(func, ms) {
+  let timer = null;
+ 
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+  }
+  return wrapper;
+}
+ 
+function debounceDecorator2(func) {
+  let timer = null;
+ 
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+    wrapper.count++;
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
